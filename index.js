d15ac4ee10716bf0e0c8cf51219e541415e6a664
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-11 20:04:46 +0700

    fix memory cache stategy

--- index.js
+++ index.js
@@ -17,57 +17,56 @@ if (typeof hexo == "undefined") {
   return;
 }
 
-if (typeof hexo != "undefined") {
-  const injector = injector2(hexo); // will make it as plugin
-  const config = getConfig(hexo);
+const injector = injector2(hexo); // will make it as plugin
+const config = getConfig(hexo);
 
-  if (config.pub.length < 1) {
-    log.error(`adsense ca-pub (adsense.pub) not configured in _config.yml`);
-    return;
-  }
+if (config.pub.length < 1) {
+  log.error(`adsense ca-pub (adsense.pub) not configured in _config.yml`);
+  return;
+}
 
-  // only apply these function on production
-  if (!isDevelopment) {
-    //log.debug("is remote");
-    // add redirect https
-    if (typeof config.https == "boolean") {
-      if (config.https) {
-        injector.register("body-end", function () {
-          const https_js = fs.readFileSync(path.join(__dirname, "source/https.js")).toString();
-          return `<script>${https_js}</script>`;
-        });
-      }
-    }
-
-    // add adblock blocker
-    if (typeof config.adblock == "boolean") {
-      if (config.adblock) {
-        const adblock_css = fs.readFileSync(path.join(__dirname, "source/adblock.css")).toString();
-        const adblock_js = fs.readFileSync(path.join(__dirname, "source/adblock.js")).toString();
-        injector.register("body-end", function () {
-          return `<style>${adblock_css}</style><script>${adblock_js}</script>`;
-        });
-      }
+// only apply these function on production
+if (!isDevelopment) {
+  //log.debug("is remote");
+  // add redirect https
+  if (typeof config.https == "boolean") {
+    if (config.https) {
+      injector.register("body-end", function () {
+        const https_js = fs.readFileSync(path.join(__dirname, "source/https.js")).toString();
+        return `<script>${https_js}</script>`;
+      });
     }
   }
 
-  injector.register("head_end", function () {
-    return `<script id="hexo-adsense-config" type="application/json">${JSON.stringify(
-      assign(config, hexo.env),
-      null,
-      2
-    )}</script>`;
+  // add adblock blocker
+  if (typeof config.adblock == "boolean") {
+    if (config.adblock) {
+      const adblock_css = fs.readFileSync(path.join(__dirname, "source/adblock.css")).toString();
+      const adblock_js = fs.readFileSync(path.join(__dirname, "source/adblock.js")).toString();
+      injector.register("body-end", function () {
+        return `<style>${adblock_css}</style><script>${adblock_js}</script>`;
+      });
+    }
+  }
+}
+
+injector.register("head_end", function () {
+  return `<script id="hexo-adsense-config" type="application/json">${JSON.stringify(
+    assign(config, hexo.env),
+    null,
+    2
+  )}</script>`;
+});
+
+if (config.enable) {
+  // adsense enabled to all pages
+  injector.register("head-end", {
+    value: `<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1165447249910969" crossorigin="anonymous"></script>`,
   });
+}
 
-  if (config.enable) {
-    // adsense enabled to all pages
-    injector.register("head-end", {
-      value: `<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1165447249910969" crossorigin="anonymous"></script>`,
-    });
-  }
-
-  if (typeof config.article_ads != "undefined") {
-    /*
+if (typeof config.article_ads != "undefined") {
+  /*
     injector.register("body-end", function () {
       let adshtml = [];
       if (Array.isArray(config.article_ads)) {
@@ -79,15 +78,14 @@ if (typeof hexo != "undefined") {
     });
     */
 
-    //log.log("hexo-adsense article ads process starting...");
-    if (config.field === "post") {
-      // only on post
-      //log.log("after_post_render");
-      hexo.extend.filter.register("after_post_render", require("./lib/article-ads").after_post_render);
-    } else {
-      //log.log("after_render:html");
-      // entire html
-      hexo.extend.filter.register("after_render:html", require("./lib/article-ads").after_render_html);
-    }
+  //log.log("hexo-adsense article ads process starting...");
+  if (config.field === "post") {
+    // only on post
+    //log.log("after_post_render");
+    hexo.extend.filter.register("after_post_render", require("./lib/article-ads").after_post_render);
+  } else {
+    //log.log("after_render:html");
+    // entire html
+    hexo.extend.filter.register("after_render:html", require("./lib/article-ads").after_render_html);
   }
 }
