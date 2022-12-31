1b40d4d1df69d76c4960797e04bf6e633f8480c8
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-31 21:04:14 +0700

    Update .prettierc.js

--- .prettierc.js
+++ .prettierc.js
@@ -1,13 +1,15 @@
 module.exports = {
+  "$schema": "https://json.schemastore.org/prettierrc",
   bracketSpacing: true,
   jsxBracketSameLine: false,
-  printWidth: 140,
+  "printWidth": 120,
   semi: true,
   singleQuote: true,
   trailingComma: "none",
   proseWrap: "never",
   arrowParens: "avoid",
   endOfLine: "lf",
+  "tabWidth": 2,
   overrides: [
     {
       files: ".prettierrc",
